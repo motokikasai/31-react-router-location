8563d2b19fc766ae3b6a7a7d6afa58244f451b92
motokikasai <kmomoky@hotmail.com>
2020-05-11 13:57:13 +0200

    Set up route to home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Location from "./pages/home/index";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Location />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Location />
+          </Route>
+          <Route>
+            <h3>404 Not Found</h3>
+            <h1>¯\_(ツ)_/¯</h1>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
