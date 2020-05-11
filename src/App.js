f56af6af0074f33c47d5240ed753aa97416eaf8a
motokikasai <kmomoky@hotmail.com>
2020-05-11 14:50:53 +0200

    Create helper function for generating obj with params

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import "./App.css";
 import Location from "./pages/home/index";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Location />
-          </Route>
-          <Route>
-            <h3>404 Not Found</h3>
-            <h1>¯\_(ツ)_/¯</h1>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Location />
+            </Route>
+            <Route>
+              <h3>404 Not Found</h3>
+              <h1>¯\_(ツ)_/¯</h1>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/helper/getSearchParams.js
@@ -0,0 +1,17 @@
+function getSearchParams(url) {
+  // eg) :3000/?customerRating=5&bookType=paperback,hardcover&language=english,german
+  const urlArr = url.slice(1).split("&");
+
+  const urlObj = urlArr.reduce((acc, curr) => {
+    const arrKeyValue = curr.split("=");
+    acc[arrKeyValue[0]] = arrKeyValue[1];
+    console.log(acc);
+    return acc;
+  }, {});
+
+  return urlObj;
+}
+
+// getSearchParams("?customerRating=5&bookType=paperback");
+
+export default getSearchParams;
